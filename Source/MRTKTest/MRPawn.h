8434ae7f94e9af7772fc2a85b1065d1e857787ce
Joseph Harvey <joe.harvey@irisndt.com>
2021-06-16 22:39:15 -0400

    initial

---
+++ Source/MRTKTest/MRPawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/DefaultPawn.h"
+#include "MRPawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MRTKTEST_API AMRPawn : public ADefaultPawn
+{
+	GENERATED_BODY()
+	
+};
